13cf08bdb33c29127643d15c642129a424e6bee9
Pance <pbarnett888@gmail.com>
2013-08-05 20:50:46 -0500

    Add missing bracket.

--- project.clj
+++ project.clj
@@ -2,5 +2,5 @@
   :description "Todo-repl"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [noir "1.3.0-beta3"]
+                 [noir "1.3.0-beta3"]]
   :main todo-repl-webapp.server)
