3dfe9808c0b01f9747e98491cb27862615f936fc
Pance <pbarnett888@gmail.com>
2013-08-12 22:02:16 -0500

    Add functions for pages with html in hiccup. Add hiccup to proj dependencies

--- project.clj
+++ project.clj
@@ -2,7 +2,8 @@
   :description "Todo-repl"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [compojure "1.1.5"]]
+                 [compojure "1.1.5"]
+                 [hiccup "1.0.4"]]
   :plugins [[lein-ring "0.8.5"]]
   :ring {:handler todo-repl-webapp.handler/app}
   :profiles
--- src/todo_repl_webapp/handler.clj
+++ src/todo_repl_webapp/handler.clj
@@ -1,10 +1,23 @@
 (ns todo-repl-webapp.handler
-  (:use compojure.core)
+  (:use compojure.core
+        hiccup.core)
   (:require [compojure.handler :as handler]
-            [compojure.route :as route]))
+            [compojure.route :as route]
+            [hiccup.form :as form]))
+
+(defn home-page [& x]
+  (html [:head [:title "todo-repl"]]
+        [:body [:h1 "todo-repl"]
+               (form/form-to [:put "/eval"]
+                        (form/text-area [] "blah")
+                        [:br]
+                        (form/submit-button "eval"))]))
+(defn eval-page [& x]
+  (html [:h1 x]))
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (GET "/" [] (home-page))
+  (PUT "/eval/:x" [x] (eval-page x))
   (route/resources "/")
   (route/not-found "Not Found"))
 
