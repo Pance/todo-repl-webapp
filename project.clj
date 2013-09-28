b7df9c53bb34c6637fe6b94ad5960f9a730a3e80
Pance <pbarnett888@gmail.com>
2013-09-28 18:55:37 -0500

    Add prettytime dependency

--- project.clj
+++ project.clj
@@ -4,9 +4,13 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [compojure "1.1.5"]
                  [hiccup "1.0.4"]
-                 [ring/ring-jetty-adapter "1.1.6"]]
+                 [ring/ring-jetty-adapter "1.1.6"]
+                 [org.ocpsoft.prettytime/prettytime-nlp "3.0.2.Final"]]
   :uberjar-name "todo-repl-webapp-standalone.jar"
   :main todo-repl-webapp.handler
+  :source-paths ["src"
+                 "src/todo_repl/"
+                 "src/todo_repl_webapp/"]
   :plugins [[lein-ring "0.8.5"]]
   :ring {:handler todo-repl-webapp.handler/app}
   :profiles
