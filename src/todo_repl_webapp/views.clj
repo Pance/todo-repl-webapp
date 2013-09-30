6260ebaf23da253774296a26eb5395960c240f9a
Pance <pbarnett888@gmail.com>
2013-09-29 19:51:51 -0500

    Improve html formatting of the tasks.
    Used some nice bootstrap.

--- src/todo_repl_webapp/views.clj
+++ src/todo_repl_webapp/views.clj
@@ -27,29 +27,28 @@
   "Given a task (map), return a div representing the task"
   [task-map]
   (core/html
-    [:div
       [:div.col-md-7.col-md-offset-1
         [:p.lead (:name task-map)]]
       [:div.col-md-2
         (if-not (nil? (:context task-map))
-          [:p (:context task-map)])]
+          [:p.text-muted (:context task-map)])]
       [:div.col-md-2
         (if-not (nil? (:due task-map))
-          [:p (:due task-map)])]]))
+          [:p.text-info (:due task-map)])]))
 
 (defn task-line-item
   "When given a single task (map), return a html table line representing the task"
   [task-map]
-  (core/html [:tr
-               [:td (task-item task-map)]]))
+  (core/html [:div.panel.panel-default
+               [:div.panel-body
+                 (task-item task-map)]]))
 
 (defn task-table 
   "When given a vector of tasks (maps), return an html table representing those tasks"
   [task-vec]
   (core/html
-    [:table.table.table-bordered
       (clojure.string/join
-        (map task-line-item task-vec))]))
+        (map task-line-item task-vec))))
 
 (defn display [x & xs]
   (if-not (empty? x)
