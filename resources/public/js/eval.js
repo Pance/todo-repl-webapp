df0479c901fcca29511ec3cbb996e827a1d98ea2
Pance <pbarnett888@gmail.com>
2013-09-29 19:15:24 -0500

    Add ability to submit input with Enter key.

--- resources/public/js/eval.js
+++ resources/public/js/eval.js
@@ -1,5 +1,4 @@
-$("#todoSubmitButton").click(function(e) {
-  e.preventDefault();
+function doEval(e) {
   $.post("/eval", { evalInput: $("#evalInput").val() } ,
                     function(response) {
     $("#display").html(response);
@@ -7,4 +6,14 @@ $("#todoSubmitButton").click(function(e) {
     .done(function() {
       $("#evalInput").val("");
     });
+}
+$("#todoSubmitButton").click(function(e) {
+  e.preventDefault();
+  doEval(e);
+});
+$('#evalInput').keydown(function(e) {
+  if (e.keyCode == 13) {
+    doEval(e);
+    return false;
+  }
 });
