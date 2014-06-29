3fff86d1153d3d5d189dce0b40338f6520a71b81
Pance <pbarnett888@gmail.com>
2014-06-28 21:52:08 -0500

    Add page at /home

--- src/todo_repl_webapp/handler.clj
+++ src/todo_repl_webapp/handler.clj
@@ -1,10 +1,15 @@
 (ns todo-repl-webapp.handler
   (:require [compojure.core :refer :all]
             [compojure.handler :as handler]
-            [compojure.route :as route]))
+            [compojure.route :as route]
+            [todo-repl-webapp.views :as views]))
+
+(def ^:dynamic *tasks-ref* (ref []))
+(defn tasks [] (deref *tasks-ref*))
 
 (defroutes app-routes
   (GET "/" [] "Hello World")
+  (GET "/home" [] (views/home (tasks)))
   (route/resources "/")
   (route/not-found "Not Found"))
 
