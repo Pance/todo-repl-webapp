2d669d3818389eb4575822eaac9b34eef139820e
Pance <pbarnett888@gmail.com>
2014-06-29 23:25:52 -0500

    Add some deps to project.clj

--- project.clj
+++ project.clj
@@ -2,7 +2,10 @@
   :description "Todo-repl"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [compojure "1.1.8"]]
+                 [compojure "1.1.8"]
+                 [ring "1.3.0"]
+                 [hiccup "1.0.4"]
+                 [org.ocpsoft.prettytime/prettytime-nlp "3.0.2.Final"]]
   :plugins [[lein-ring "0.8.11"]]
   :ring {:handler todo-repl-webapp.handler/app}
   :profiles
