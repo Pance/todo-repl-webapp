5558caf8241caaecf09e2c80c52383fe33082dee
Pance <pbarnett888@gmail.com>
2014-06-29 23:05:30 -0500

    Add handler fn for a POST form. Moved todo_repl files from old src folder to new one

--- src/todo_repl_webapp/handler.clj
+++ src/todo_repl_webapp/handler.clj
@@ -10,6 +10,15 @@
 (defroutes app-routes
   (GET "/" [] "Hello World")
   (GET "/home" [] (views/home (tasks)))
+  (POST "/eval" [evalInput]
+    (do 
+      (println "/eval " evalInput)
+      (if (nil? evalInput)
+        (views/display "nil")
+          (binding [*ns* (find-ns 'todo-repl-webapp.web-repl)]
+            (let [result (load-string evalInput)]
+              (println "evals to: " result)
+              (views/display result))))))
   (route/resources "/")
   (route/not-found "Not Found"))
 
