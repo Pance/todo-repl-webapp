59de495d4f033b525eabf44771b30b8b7fe13caf
Pance <pbarnett888@gmail.com>
2014-07-08 19:43:17 -0500

    Add an app handler secured by friend

--- src/todo_repl_webapp/handler.clj
+++ src/todo_repl_webapp/handler.clj
@@ -2,8 +2,12 @@
   (:require [compojure.core :refer :all]
             [compojure.handler :as handler]
             [compojure.route :as route]
+            [cemerick.friend :as friend]
+            (cemerick.friend [workflows :as workflows]
+                             [credentials :as credentials])
             [todo-repl-webapp.views :as views]
             [todo-repl-webapp.eval :as e]
+            [todo-repl-webapp.users :as u]
             [todo-repl-webapp.web-repl :as web-repl]))
 
 (defroutes app-routes
@@ -18,3 +22,9 @@
 
 (def app
   (handler/site app-routes))
+
+(def secured-app
+  (-> app
+    (friend/authenticate {:credential-fn
+                            (partial credentials/bcrypt-credential-fn u/users)
+                          :workflows [(workflows/interactive-form)]})))
